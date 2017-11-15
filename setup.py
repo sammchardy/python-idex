cab866871b1f22da9074d98c0f0e6a8c16e3de32
Sam McHardy <hello@sammchardy.net>
2017-11-15 11:04:44 +0930

    Add initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='python-idex',
+    version='0.1.0',
+    packages=['idex'],
+    description='IDEX REST API python implementation',
+    url='https://github.com/sammchardy/python-idex',
+    author='Sam McHardy',
+    license='MIT',
+    author_email='',
+    install_requires=['requests'],
+    keywords='idex exchange rest api ethereum eth eos',
+    classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
